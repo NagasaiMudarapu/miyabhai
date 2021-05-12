9214d244c8825ca4e2bdd4affcddf5e0d28f4979
NagasaiMudarapu <nagasaimudarapu74@gmail.com>
2021-05-12 21:13:18 +0530

    JKLMN

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express') ;
+const app = express() ;
+app.use(express.static(__dirname + '/frontend')) ;
+app.get('/', function(req, res)
+{
+    res.sendFile(__dirname + '/frontend/html/home.html') ;
+}) ;
+
+var PORT = process.env.PORT || 3000 ;
+app.listen(PORT, function()
+{
+    console.log("Site running on https:localhost:" + PORT) ;
+}) ;
